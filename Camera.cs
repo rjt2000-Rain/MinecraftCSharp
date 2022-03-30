99c484ba0e82168f5efcb3b1b430ecc3ff7c57f3
Ryan Tryba <rtryba@precoinc.com>
2022-03-30 09:11:39 -0500

    Testing

---
+++ Camera.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace OpenGLTesting
+{
+    class Camera
+    {
+        public Vector3D look = new Vector3D(1,0,0);
+        public Vector3D pos = new Vector3D(0, 0, 0);
+        private Vector3D vel = new Vector3D(0, 0, 0);
+
+        private const float Rotate_Deg = 0.1f;
+        private readonly Vector3D Max_Vel = new Vector3D(5, 4, 5);
+        private readonly float Rotate_X = (float)Math.Cos(Rotate_Deg);
+        private readonly float Rotate_Y = (float)Math.Sin(Rotate_Deg);
+        private readonly float Rotate_X_Neg = (float)Math.Cos(-Rotate_Deg);
+        private readonly float Rotate_Y_Neg = (float)Math.Sin(-Rotate_Deg);
+        private readonly Vector3D Vert_Accel = new Vector3D(0, 0.1, 0);
+        private readonly Vector3D Strafe_Accel = new Vector3D(0.05, 0, 0);
+        private readonly Vector3D Walk_Accel = new Vector3D(0, 0, 0.05);
+
+        public Camera()
+        {
+
+        }
+
+        public void Update()
+        {
+            if(vel.LengthSquared > Max_Vel.LengthSquared)
+            {
+                int sign = (vel.LengthSquared >= 0) ? 1 : -1;
+                vel = Max_Vel;
+                vel = Vector3D.Multiply(sign, vel);
+            }
+            pos = Vector3D.Add(pos, vel);
+            Vector3D damping = new Vector3D(vel.X, vel.Y, vel.Z);
+            damping.Normalize();
+            vel = Vector3D.Subtract(vel, damping);
+        }
+
+        internal void LookRight()
+        {
+            double oldX = look.X;
+            double oldZ = look.Z;
+
+            look.X = oldX * Math.Cos(Rotate_Deg) - oldZ * Math.Sin(Rotate_Deg);
+            look.Z = oldX * Math.Sin(Rotate_Deg) + oldZ * Math.Cos(Rotate_Deg);
+        }
+
+        internal void LookLeft()
+        {
+            double oldX = look.X;
+            double oldZ = look.Z;
+
+            look.X = oldX * Math.Cos(-Rotate_Deg) - oldZ * Math.Sin(-Rotate_Deg);
+            look.Z = oldX * Math.Sin(-Rotate_Deg) + oldZ * Math.Cos(-Rotate_Deg);
+        }
+
+        internal void MoveForward()
+        {
+            Vector3D forward = new Vector3D(look.X, 0, look.Z);
+            forward = Vector3D.Add(forward, Walk_Accel);
+            forward.Normalize();
+            vel = Vector3D.Add(vel, forward);
+        }
+
+        internal void MoveBackward()
+        {
+            Vector3D backward = new Vector3D(look.X, 0, look.Z);
+            backward = Vector3D.Subtract(backward, Walk_Accel);
+            backward.Normalize();
+            vel = Vector3D.Add(vel, backward);
+        }
+
+        internal void MoveRight()
+        {
+            Vector3D rightward = new Vector3D(look.X, 0, look.Z);
+            rightward = Vector3D.Subtract(rightward, Walk_Accel);
+            rightward.Normalize();
+            vel = Vector3D.Add(vel, rightward);
+        }
+
+        internal void MoveLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void MoveUp()
+        {
+            pos = Vector3D.Add(pos, Up_Move);
+        }
+
+        internal void MoveDown()
+        {
+            pos = Vector3D.Add(pos, Down_Move);
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,8 +10,11 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SharpGL;
+using SharpGL.WPF;
 
 namespace MinecraftSharp
 {
@@ -20,11 +23,153 @@ namespace MinecraftSharp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Camera cam;
         private World world;
+
         public MainWindow()
         {
+            Height = 450;
+            Width = 800;
+
+            cam = new Camera();
+
             InitializeComponent();
             world = new World();
+        }
+
+        private void sharpControl_OpenGLInitialized(object sender, OpenGLRoutedEventArgs args)
+        {
+            //  Get the OpenGL object.
+            OpenGL gl = sharpControl.OpenGL;
+
+            //  Set the clear color.
+            gl.ClearColor(0, 0, 0, 0);
+        }
+
+        private void sharpControl_Resized(object sender, OpenGLRoutedEventArgs args)
+        {
+            //  Get the OpenGL object.
+            OpenGL gl = sharpControl.OpenGL;
+
+            //  Set the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+
+            //  Load the identity.
+            gl.LoadIdentity();
+
+            //  Create a perspective transformation.
+            gl.Perspective(80.0f, (double)Width / (double)Height, 0.01f, 100.0f);
+
+            //  Use the 'look at' helper function to position and aim the camera.
+            gl.LookAt(2, 2, 2, 0, 0, 0, 0, 1, 0);
+
+            //  Set the modelview matrix.
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+        }
+
+        private void sharpControl_OpenGLDraw(object sender, OpenGLRoutedEventArgs args)
+        {
+            cam.Update();
+
+            //  Get the OpenGL object.
+            OpenGL gl = sharpControl.OpenGL;
+
+            //  Clear the color and depth buffer.
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            gl.LoadIdentity();
+            //gl.Rotate();
+            //gl.Translate(cam.pos.X, -cam.pos.Y, cam.pos.Z);
+
+            //  Draw a coloured pyramid.
+            gl.Begin(SharpGL.Enumerations.BeginMode.Quads);
+
+            gl.Color(1f, 0f, 0f);
+            gl.Vertex(0f, 0f, 0f);
+            gl.Vertex(1f, 0f, 0f);
+            gl.Vertex(1f, 1f, 0f);
+            gl.Vertex(0f, 1f, 0f);
+
+            gl.Color(0f, 0f, 1f);
+            gl.Vertex(0f, 0f, 0f);
+            gl.Vertex(0f, 0f, 1f);
+            gl.Vertex(0f, 1f, 1f);
+            gl.Vertex(0f, 1f, 0f);
+
+            gl.Color(0f, 1f, 0f);
+            gl.Vertex(0f, 0f, 0f);
+            gl.Vertex(1f, 0f, 0f);
+            gl.Vertex(1f, 0f, 1f);
+            gl.Vertex(0f, 0f, 1f);
+
+            gl.End();
+
+            gl.Begin(SharpGL.Enumerations.BeginMode.Lines);
+
+            gl.Color(0, 0, 0);
+            gl.Vertex(0, 0, 0);
+
+            gl.Color(0f, 1f, 1f);
+            gl.Vertex(0, 0, 3);
+
+            gl.Color(0, 0, 0);
+            gl.Vertex(0, 0, 0);
+
+            gl.Color(1f, 0f, 1f);
+            gl.Vertex(0, 3, 0);
+
+            gl.Color(0, 0, 0);
+            gl.Vertex(0, 0, 0);
+
+            gl.Color(1f, 1f, 0f);
+            gl.Vertex(3, 0, 0);
+
+            gl.End();
+
+            gl.Flush();
+        }
+
+        private void ClearView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.E)
+            {
+                cam.LookRight();
+            }
+
+            if (e.Key == Key.Q)
+            {
+                cam.LookLeft();
+            }
+
+            if (e.Key == Key.W)
+            {
+                cam.MoveForward();
+            }
+
+            if (e.Key == Key.S)
+            {
+                cam.MoveBackward();
+            }
+
+            if (e.Key == Key.D)
+            {
+                cam.MoveRight();
+            }
+
+            if (e.Key == Key.A)
+            {
+                cam.MoveLeft();
+            }
+
+            if (e.Key == Key.Space)
+            {
+                cam.MoveUp();
+            }
+
+            if (e.Key == Key.LeftShift)
+            {
+                cam.MoveDown();
+            }
         }
     }
 }
